b18e6610f7d438e815bc855e167f053f3b781857
Anthony <info@anthonyscheeres.nl>
2020-12-01 14:53:03 +0100

    Made sampel models

---
+++ app/src/main/java/com/example/isensiotapplication/service/models/Interval.java
@@ -0,0 +1,11 @@
+package com.example.isensiotapplication.service.models;
+
+
+//{"time": %d, "ultrasoonSensor":%d, "lightSensor":&d, "moistureSensor":&d}
+public class Interval {
+    String time;
+    String ultrasoonSensor;
+    String lightSensor;
+    String moistureSensor;
+
+}
---
+++ app/src/main/java/com/example/isensiotapplication/service/models/IntervalCollection.java
@@ -0,0 +1,9 @@
+package com.example.isensiotapplication.service.models;
+
+import java.util.List;
+
+public class IntervalCollection {
+    List<Interval> intervals;
+
+
+}
